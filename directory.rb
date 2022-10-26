2d6eed81cd695a3a05a922fdcfc4c9bef189483b
MthBarber <mthbarber@gmail.com>
2022-10-26 16:11:56 +0000

    Put some actions inside methods and call them inside directory.rb

--- directory.rb
+++ directory.rb
@@ -12,12 +12,22 @@ students = [
  "Joffrey Baratheon",
  "Norman Bates"
  ]
- #then print them
- puts "The students of Villains Academy"
- puts "----------"
- students.each do |student|
-    puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
 end
-#finally we print the total number of students
-print "Overall, we have #{students.count} great students"
-#its import that print() doesn't add new line characters
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
